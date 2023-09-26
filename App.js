9a7afd131cbc1a82d28a58e7f0ff72597534ebbb
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2023-09-26 10:35:54 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HelloWorldApp  from 'components/HelloWorld';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <HelloWorldApp />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    text-size: 
+  },
+});
